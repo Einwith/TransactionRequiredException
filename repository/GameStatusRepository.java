ea1c2e7e7ccb211e2b2988d314f608f0d447ccc7
TZhang <35837668+Einwith@users.noreply.github.com>
2022-04-12 09:57:47 -0300

    Delete repository directory

--- repository/GameStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lixar.apba.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lixar.apba.domain.GameStatus;
-
-public interface GameStatusRepository extends JpaRepository<GameStatus, Integer> {
-	GameStatus findOneByGame(Integer gameId);
-}
--- repository/SystemNodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lixar.apba.repository;
-
-
-import com.lixar.apba.domain.SystemNode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SystemNodeRepository extends JpaRepository<SystemNode, Integer> {
-    SystemNode findByIp(String ip);
-}
